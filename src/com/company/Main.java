723c51184f19bc3d04f2a1a5eabbb2b64a82636d
Alexey Koshelivskiy <avk21.me@gmail.com>
2021-12-17 16:56:35 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GitHub test");
         System.out.println("Hi gitHub!");
+        System.out.println("I'm testing again!");
     }
 }
